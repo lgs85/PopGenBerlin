2cd031f31a472126a7be4097e57e50053c0ea301
lgs85 <lewisspurgin@gmail.com>
2018-05-16 08:48:24 +0100

    Typo fixing

--- Lectures/Intro_to_R/Mainland_Island_SNP_analysis.R
+++ Lectures/Intro_to_R/Mainland_Island_SNP_analysis.R
@@ -1,5 +1,5 @@
 #Read in data
-dd <- read.csv("../Data/Mainland_Island_SNP_data.csv")
+dd <- read.csv("Mainland_Island_SNP_data.csv")
 
 #Create a heterozygote/homozygote column
 dd$Het <- ifelse(dd$Allele_1 == dd$Allele_2,"Homozygote","Heterozygote")
@@ -12,3 +12,20 @@ barplot(het_freqs,ylab = "Proportion heterozygotes")
 
 #Perform a Chi-squared test
 chisq.test(dd$Het,dd$Pop)
+
+#Allele frequencies
+a1 <- table(dd$Allele_1,dd$Pop)
+a2 <- table(dd$Allele_2,dd$Pop)
+
+a_counts <- a1+a2
+
+Island_freq <- a_counts[,"Island"]/sum(a_counts[,"Island"])
+Mainland_freq <- a_counts[,"Mainland"]/sum(a_counts[,"Mainland"])
+
+newdata <- cbind(Island_freq,Mainland_freq)
+
+barplot(newdata)
+
+
+
+
